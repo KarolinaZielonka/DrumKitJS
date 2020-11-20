846293e68253eb2a2f1d5b2c0e17b01ebda869cf
Karolina <k99zielonka@gmail.com>
2020-11-20 20:10:59 +0100

    CodeForDrumKit
    Simple web with sound.
    Using JS, CSS and HTML. After clicking each buton sound is playing.

---
+++ app.js
@@ -0,0 +1,16 @@
+window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //bad key == null
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing');
+});
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
